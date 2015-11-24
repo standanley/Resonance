26b47c092c54b3df911b5876189b4241ea931e7a
Daniel <Daniel@Stanley5.com>
2015-11-24 14:38:55 -0500

    Modified Viewer.java to display group 7.

--- Physics Project/src/Viewer.java
+++ Physics Project/src/Viewer.java
@@ -3,10 +3,10 @@ public class Viewer {
 
         //StdDraw.picture(.5, .5, "froup1/pic30");
 
-        for (int i = 0; i < 1355; i++) {
+        for (int i = 0; i < 99; i++) {
             System.out.println(i);
             if (i != 36) {
-                StdDraw.picture(.5, .5, "group8/pic" + i + ".png");
+                StdDraw.picture(.5, .5, "group7/pic" + i + ".png");
                 StdDraw.show(66);
             }
         }
